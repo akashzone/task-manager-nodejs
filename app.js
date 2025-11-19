e02b4787193124fc9ed12f582272abaee66b7b49
akashzone <akashnadar.dev@gmail.com>
2025-11-19 20:02:01 +0530

    Enhanced UX by adding inline add-task input on homepage

--- app.js
+++ app.js
@@ -2,17 +2,15 @@
 const express = require("express");
 require("dotenv").config();
 
-
 const app = express();
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 
 const port = process.env.PORT || 8080;
-
 
 const mysql = require("mysql2");
 
@@ -21,9 +19,8 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
 });
-
 
 connection.connect((err) => {
   if (err) {
@@ -38,44 +35,41 @@ app.get("/", (req, res) => {
   let q = "SELECT * from tasks";
   try {
     connection.query(q, (err, result) => {
-      res.render("home",{users : result});
+      res.render("home", { tasks: result });
     });
   } catch (error) {
     console.log("Error:" + error);
-    res.send("Error occured!")
+    res.send("Error occured!");
   }
 });
 
 app.post("/add-tasks", (req, res) => {
-  
-  let { task } = req.body;  
-  if(task === ""){
-    res.send("Please enter a valid task!");
-    return;
+  const task = req.body.task;
+  console.log(task);
+  if (!task) {
+    return res.json({ success: false });
   }
-  // console.log(task);
-
   let q = "INSERT INTO tasks(title) VALUES (?)";
 
   connection.query(q, [task], (err, result) => {
-    if (err) throw err;
+    if (err) {
+      return res.json({ success: false });
+    }
     console.log(result);
-    res.redirect("/");
+    res.json({ success: true, id: result.insertId });
   });
 });
 
 app.post("/complete/:id", (req, res) => {
   const id = req.params.id;
-
   const q = "UPDATE tasks SET is_completed = NOT is_completed WHERE id = ?";
-
   connection.query(q, [id], () => {
-    res.redirect("/");
-    });
+    res.json({ success: true });
   });
+});
 
 
-app.get("/delete/:id",async (req, res) => {
+app.get("/delete/:id",(req, res) => {
   const id = req.params.id;
   const q = "DELETE FROM tasks WHERE id = ?";
 
@@ -85,7 +79,7 @@ app.get("/delete/:id",async (req, res) => {
   });
 });
 
-app.get("/delete-all",async (req, res) => {
+app.get("/delete-all", async (req, res) => {
   const q = "TRUNCATE TABLE tasks";
 
   connection.query(q, (err, result) => {
@@ -94,9 +88,6 @@ app.get("/delete-all",async (req, res) => {
   });
 });
 
-app.get("/add-tasks",(req,res)=>{
-    res.render("add");
-});
 app.listen(port, () => {
   console.log("Server is live");
 });
--- public/script.js
+++ public/script.js
@@ -18,3 +18,75 @@ toggleBtn.addEventListener("click", () => {
     localStorage.setItem("theme", "light");
   }
 });
+
+
+const form = document.getElementById("taskForm");
+const input = document.getElementById("taskInput");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const task = input.value.trim();
+  console.log(task);
+  try {
+    const response = await fetch("/add-tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ task }),
+    });
+    const data = await response.json();
+    // alert(result);
+    
+    if (data.success) {
+    addTaskToScreen(task, data.id);
+}
+
+    input.value = ""; 
+  } catch (error) {
+    console.log("Error:" + error);
+    alert("Error occurred!");
+  }
+});
+
+function addTaskToScreen(name, id) {
+  const list = document.getElementById("taskList");
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <input type="checkbox" class="task-check" data-id="${id}" />
+    
+    <span class="task-text">${name}</span>
+
+    <form action="/delete/${id}" method="get" style="display:inline;">
+      <button type="submit" id="del-btn">Remove</button>
+    </form>
+  `;
+
+  list.appendChild(li);
+}
+
+let checkboxes = document.querySelectorAll(".task-check");
+
+document.addEventListener("change", async (e) => {
+  function addTaskToScreen(name, id) {
+  const list = document.getElementById("taskList");
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <div class="task-left">
+      <input type="checkbox" class="task-check" data-id="${id}" />
+      <span class="task-text">${name}</span>
+    </div>
+
+    <form action="/delete/${id}" method="get" style="display:inline;">
+      <button type="submit" id="del-btn">Remove</button>
+    </form>
+  `;
+
+  list.appendChild(li);
+}
+
+});
